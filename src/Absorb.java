3aae865bcc94d82c5069aeb26fe4285c9efc4de1
Barry Adams <barry.david.adams@gmail.com>
2022-01-02 22:38:46 +0000

    master add nair constants

--- src/Absorb.java
+++ src/Absorb.java
@@ -6,35 +6,34 @@ public class Absorb {
 
 
     double absorbC02(double concentration, double freq, double lat, double height, double heightStep, int j){
-        double c1 = crossSec(freq, height, lat, Constants.LINE1_C02_EINSTEIN_A , Constants.LINE1_CO2_WN, Constants.LINE1_CO2_MULTIPLICITY, Constants.LINE1_CO2_WIDTH);
-        double c2 = crossSec(freq, height, lat,Constants.LINE2_CO2_EINSTEIN_A , Constants.LINE2_CO2_WN, Constants.LINE2_CO2_MULTIPLICITY, Constants.LINE2_CO2_WIDTH);
-        double c3 = crossSec(freq, height, lat, Constants.LINE3_CO2_EINSTEIN_A, Constants.LINE3_CO2_WN, Constants.LINE3_CO2_MULTIPLICITY, Constants.LINE3_CO2_WIDTH);
+        double c1 = crossSec(freq, height, lat, Constants.LINE1_C02_EINSTEIN_A , Constants.LINE1_CO2_WN, Constants.LINE1_CO2_MULTIPLICITY, Constants.LINE1_CO2_WIDTH, Constants.LINE1_CO2_NAIR);
+        double c2 = crossSec(freq, height, lat,Constants.LINE2_CO2_EINSTEIN_A , Constants.LINE2_CO2_WN, Constants.LINE2_CO2_MULTIPLICITY, Constants.LINE2_CO2_WIDTH, Constants.LINE2_CO2_WIDTH);
+        double c3 = crossSec(freq, height, lat, Constants.LINE3_CO2_EINSTEIN_A, Constants.LINE3_CO2_WN, Constants.LINE3_CO2_MULTIPLICITY, Constants.LINE3_CO2_WIDTH, Constants.LINE3_CO2_WIDTH);
         return Math.exp(heightStep*concentration*(c1+c2+c3));
     }
 
     double absorbH02(double concentration, double freq, double lat, double height, double heightStep, int j){
-        double h1 = crossSec(freq, height, lat, Constants.LINE1_C02_EINSTEIN_A , Constants.LINE1_H20_WN, Constants.LINE1_H20_MULTIPLICITY, Constants.LINE1_H20_WIDTH);
-        double h2 = crossSec(freq, height, lat,Constants.LINE2_H20_EINSTEIN_A , Constants.LINE2_H20_WN, Constants.LINE2_H20_MULTIPLICITY, Constants.LINE2_H20_WIDTH);
-        double h3 = crossSec(freq, height, lat, Constants.LINE3_H20_EINSTEIN_A, Constants.LINE3_H20_WN, Constants.LINE3_H20_MULTIPLICITY, Constants.LINE3_H20_WIDTH);
-        double h4 =  crossSec(freq, height, lat, Constants.LINE4_H20_EINSTEIN_A, Constants.LINE4_H20_WN, Constants.LINE4_H20_MULTIPLICITY, Constants.LINE4_H20_WIDTH);
+        double h1 = crossSec(freq, height, lat, Constants.LINE1_C02_EINSTEIN_A , Constants.LINE1_H20_WN, Constants.LINE1_H20_MULTIPLICITY, Constants.LINE1_H20_WIDTH, Constants.LINE1_H20_NAIR);
+        double h2 = crossSec(freq, height, lat,Constants.LINE2_H20_EINSTEIN_A , Constants.LINE2_H20_WN, Constants.LINE2_H20_MULTIPLICITY, Constants.LINE2_H20_WIDTH, Constants.LINE2_H20_NAIR);
+        double h3 = crossSec(freq, height, lat, Constants.LINE3_H20_EINSTEIN_A, Constants.LINE3_H20_WN, Constants.LINE3_H20_MULTIPLICITY, Constants.LINE3_H20_WIDTH, Constants.LINE3_H20_NAIR);
+        double h4 =  crossSec(freq, height, lat, Constants.LINE4_H20_EINSTEIN_A, Constants.LINE4_H20_WN, Constants.LINE4_H20_MULTIPLICITY, Constants.LINE4_H20_WIDTH, Constants.LINE4_H20_NAIR);
         return Math.exp(-heightStep*concentration*(h1+h2+h3+h4));
     }
 
     double absorbAll(double concentration, double freq, double lat, double height, double heightStep, int j){
-        double h1 = crossSec(freq, height, lat, Constants.LINE1_C02_EINSTEIN_A , Constants.LINE1_H20_WN, Constants.LINE1_H20_MULTIPLICITY, Constants.LINE1_H20_WIDTH);
-        double h2 = crossSec(freq, height, lat,Constants.LINE2_H20_EINSTEIN_A , Constants.LINE2_H20_WN, Constants.LINE2_H20_MULTIPLICITY, Constants.LINE2_H20_WIDTH);
-        double h3 = crossSec(freq, height, lat, Constants.LINE3_H20_EINSTEIN_A, Constants.LINE3_H20_WN, Constants.LINE3_H20_MULTIPLICITY, Constants.LINE3_H20_WIDTH);
-        double h4 =  crossSec(freq, height, lat, Constants.LINE4_H20_EINSTEIN_A, Constants.LINE4_H20_WN, Constants.LINE4_H20_MULTIPLICITY, Constants.LINE4_H20_WIDTH);
-        double c1 = crossSec(freq, height, lat, Constants.LINE1_C02_EINSTEIN_A , Constants.LINE1_CO2_WN, Constants.LINE1_CO2_MULTIPLICITY, Constants.LINE1_CO2_WIDTH);
-        double c2 = crossSec(freq, height, lat,Constants.LINE2_CO2_EINSTEIN_A , Constants.LINE2_CO2_WN, Constants.LINE2_CO2_MULTIPLICITY, Constants.LINE2_CO2_WIDTH);
-        double c3 = crossSec(freq, height, lat, Constants.LINE3_CO2_EINSTEIN_A, Constants.LINE3_CO2_WN, Constants.LINE3_CO2_MULTIPLICITY, Constants.LINE3_CO2_WIDTH);
+        double h1 = crossSec(freq, height, lat, Constants.LINE1_C02_EINSTEIN_A , Constants.LINE1_H20_WN, Constants.LINE1_H20_MULTIPLICITY, Constants.LINE1_H20_WIDTH, Constants.LINE1_H20_NAIR);
+        double h2 = crossSec(freq, height, lat,Constants.LINE2_H20_EINSTEIN_A , Constants.LINE2_H20_WN, Constants.LINE2_H20_MULTIPLICITY, Constants.LINE2_H20_WIDTH, Constants.LINE2_H20_NAIR);
+        double h3 = crossSec(freq, height, lat, Constants.LINE3_H20_EINSTEIN_A, Constants.LINE3_H20_WN, Constants.LINE3_H20_MULTIPLICITY, Constants.LINE3_H20_WIDTH, Constants.LINE3_H20_NAIR);
+        double h4 =  crossSec(freq, height, lat, Constants.LINE4_H20_EINSTEIN_A, Constants.LINE4_H20_WN, Constants.LINE4_H20_MULTIPLICITY, Constants.LINE4_H20_WIDTH, Constants.LINE4_H20_NAIR);
+        double c1 = crossSec(freq, height, lat, Constants.LINE1_C02_EINSTEIN_A , Constants.LINE1_CO2_WN, Constants.LINE1_CO2_MULTIPLICITY, Constants.LINE1_CO2_WIDTH, Constants.LINE1_CO2_NAIR);
+        double c2 = crossSec(freq, height, lat,Constants.LINE2_CO2_EINSTEIN_A , Constants.LINE2_CO2_WN, Constants.LINE2_CO2_MULTIPLICITY, Constants.LINE2_CO2_WIDTH, Constants.LINE2_CO2_WIDTH);
+        double c3 = crossSec(freq, height, lat, Constants.LINE3_CO2_EINSTEIN_A, Constants.LINE3_CO2_WN, Constants.LINE3_CO2_MULTIPLICITY, Constants.LINE3_CO2_WIDTH, Constants.LINE3_CO2_WIDTH);
         return Math.exp(-heightStep*concentration*(h1+h2+h3+h4+c1+c2+c3));
     }
 
-    static double crossSec(double freq, double lat, double height, double A, double WN, double g, double gammaair){
+    static double crossSec(double freq, double lat, double height, double A, double WN, double g, double gammaair, double nair){
         double pressure = 1.0;
         double temp = 296.0;
-        double nair = 1.0;
         double width = LineWidth.halfwidth(pressure, temp, nair, gammaair);
         double freq1 = WN*Constants.SPEED_OF_LIGHT_CM;
         double cross = LorentzAbsorb.sigma(freq, freq1, width);
